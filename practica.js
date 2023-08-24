15b333d81df3aa97fc328d9d5e6ba31535097727
Elias Galvis <eljosegla@gmail.com>
2023-08-24 18:09:41 +0200

    Primera practica

---
+++ practica.js
@@ -0,0 +1,21 @@
+let saludar = "Hola mundo";
+console.log(saludar);
+console.log(
+    saludar.length
+);
+
+var cadena1 = "hola mundo";
+var cadena2 = cadena1.slice(0, -5);
+console.log(cadena2);
+
+
+var miCadena = 'hola que tal';
+var divisiones = miCadena.split(" ", 3);
+
+print(divisiones);
+
+console.log(divisiones);
+
+let cadena = "Hola Mundo "; // con espacio al final
+let cadenaRepetida = cadena.repeat(3);
+console.log(cadenaRepetida);
